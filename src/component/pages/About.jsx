b322f1937a1e587537416155f261df11828f9ae5
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-05-20 11:32:00 +0530

    Removed layoutBy in the about

--- src/component/pages/About.jsx
+++ src/component/pages/About.jsx
@@ -18,10 +18,7 @@ function About() {
       Version <span className='text-white'>1.0.0</span>
     </p>
     <p className='text-lg text-gray-400'>
-      Layout By:
-      <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-        Hassib Moddasser
-      </a>
+
     </p>
   </div>;
 }
